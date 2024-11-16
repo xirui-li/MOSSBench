9b04806166288ddd9713e0b703dd96083a8d2dc6
Xirui Li <xiruili@g.ucla.edu>
2024-11-16 11:06:55 -0800

    Update __init__.py

--- MOSSBench/models/__init__.py
+++ MOSSBench/models/__init__.py
@@ -4,4 +4,4 @@ from .openai_model import OpenaiModel
 from .google_model import GoogleModel
 from .load_model import load_model
 
-__all__ = ['ModelBase', 'WhiteBoxModelBase', 'BlackBoxModelBase', 'HuggingfaceModel', 'from_pretrained', 'OpenaiModel', 'WenxinyiyanModel', 'GoogleModel']
+__all__ = ['ModelBase', 'WhiteBoxModelBase', 'BlackBoxModelBase', 'HuggingfaceModel', 'from_pretrained', 'OpenaiModel', 'GoogleModel']
